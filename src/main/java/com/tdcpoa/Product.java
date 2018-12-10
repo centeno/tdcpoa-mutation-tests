c97b033540c74159dc26954891fab070c726b942
Ricardo Gaete Duarte <ricardo.gaete@latam.com>
2018-12-10 15:47:58 -0300

    Adding code made in presentation

--- src/main/java/com/tdcpoa/Product.java
+++ src/main/java/com/tdcpoa/Product.java
@@ -2,4 +2,11 @@ package com.tdcpoa;
 
 public class Product {
 
+    public int getPrice(int amount, boolean coupon){
+        if(amount >= 20 || coupon){
+            return amount * 15;
+        }
+        return amount * 17;
+    }
+
 }
--- src/test/java/com/tdcpoa/ProductTest.java
+++ src/test/java/com/tdcpoa/ProductTest.java
@@ -4,5 +4,40 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ProductTest {
-  
+
+    @Test
+    public void shouldReturnNormalPrice() {
+        Product product = new Product();
+        int amount = 2;
+        int price = product.getPrice(amount,false);
+        assertEquals(34,price);
+    }
+
+    @Test
+    public void shouldReturnPriceByAmount() {
+        Product product = new Product();
+        int amount = 100;
+        int price = product.getPrice(amount, false);
+        assertEquals(1500,price);
+    }
+
+
+    @Test
+    public void shouldReturnPriceByCoupon() {
+        Product product = new Product();
+        int amount = 10;
+        int price = product.getPrice(amount, true);
+        assertEquals(150,price);
+    }
+
+
+    @Test
+    public void shouldReturnPriceByAmount20() {
+        Product product = new Product();
+        int amount = 20;
+        int price = product.getPrice(amount, false);
+        assertEquals(300,price);
+    }
+
+
 }
